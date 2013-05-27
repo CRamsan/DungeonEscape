34423835759435a0c545b1c1750c4d7f49f9b20f
Cesar Ramirez <ramirez@cesarandres.com>
2013-05-27 05:15:07 -0500

    Vector class translated to python2

--- src/util.py
+++ src/util.py
@@ -1,5 +1,44 @@
 import string
 import random
+import math
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for x in range(size))
+
+class Vector():
+        
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+            self.mag = self.magnitude
+            
+        def magnitude(self):
+            return math.sqrt(self.x * self.x + self.y * self.y)
+        
+
+        def normalize(self):
+            self.mag = self.magnitude
+            self.x = self.x / self.mag
+            self.y = self.y / self.mag
+
+        def clone_normalized(self):
+            return Vector(self.x / self.magnitude(), self.y / self.magnitude());
+
+        def dot_product(self, vector):
+            return self.x * vector.x + self.y * vector.y
+
+        def distance_to(self, vector):
+            return math.sqrt(math.pow(vector.x - self.x, 2) + math.pow(vector.y - self.y, 2))
+
+        def __add__(self, vector_a, vector_b):
+            return Vector(vector_a.x + vector_b.x, vector_a.y + vector_b.y);
+
+        def __sub__(self, vector_a, vector_b):
+            return Vector(vector_a.x - vector_b.x, vector_a.y - vector_b.y);
+
+        def __mul__(self, vector_a, float):
+            return Vector(vector_a.x * float, vector_a.y * float);
+
+        def equals(self, object):
+            return self.x == object.x and self.y == object.y
+
